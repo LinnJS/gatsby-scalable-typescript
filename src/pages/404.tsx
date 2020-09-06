215ba7dcad626faa3d94c156611d6b602abf9ff6
LinnJS <justinslinn727@gmail.com>
2020-09-06 16:53:30 -0400

    Add return types

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 import Layout from 'src/global/Layout';
 
-const NotFoundPage = () => (
+const NotFoundPage = (): ReactNode => (
   <Layout>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
